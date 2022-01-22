35342e84356652d863c32f0e8f85c9aa7e4c21d5
rens <rein.satyawarman@gmail.com>
2022-01-22 14:14:20 +0700

    MatHax Concept

---
+++ assets/mathax/script.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 20) nav.classList.add('scrolled');
+    else nav.classList.remove('scrolled');
+});
+
+
+window.onload = function() {
+    $('.loader-cont').remove();
+    $('body').removeClass('loading');
+}
+
+$('#go-down').click(function (e) {
+    $([document.documentElement, document.body]).animate(
+        {
+            scrollTop: $('.announcement').offset().top - $('nav').height(),
+        },
+        1000
+    );
+});
